70455f148cb75cde8eafeaaffc44428038aee70f
Day4me23 <Dayneruns5k@gmail.com>
2022-08-26 23:21:02 -0500

    Flashlight Mouse Follow Script

--- BrackeysJam22/Assets/Scripts/FlashlightPivot.cs
+++ BrackeysJam22/Assets/Scripts/FlashlightPivot.cs
@@ -8,6 +8,10 @@ public class FlashlightPivot : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
     }
 }
